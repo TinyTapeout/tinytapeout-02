73bb1d3ce180adcd24450f929f54bd0b38e9c31d
matt venn <matt@mattvenn.net>
2022-11-12 18:17:51 +0100

    fetch list of git urls

---
+++ update_urls.py
@@ -0,0 +1,12 @@
+import stripe, os
+stripe.api_key = os.environ['STRIPE_TOKEN']
+checkouts = stripe.checkout.Session.list(limit=400)
+git_urls = []
+for checkout in checkouts:
+    if checkout['payment_status'] == 'paid':
+        if 'github' in checkout['metadata']:
+            git_urls.append(checkout['metadata']['github'])
+
+git_urls.reverse()
+for git_url in git_urls:
+    print(git_url)
