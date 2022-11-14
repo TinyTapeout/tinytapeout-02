bcf653fcf0cc9192675eabe1be5d5dcb751032b6
Matt Venn <matt@mattvenn.net>
2022-11-14 15:35:39 +0100

    auto create project_urls.py

--- update_urls.py
+++ update_urls.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python3
 import stripe, os
 stripe.api_key = os.environ['STRIPE_TOKEN']
 checkouts = stripe.checkout.Session.list(limit=400)
+
+with open("project_urls_init.py") as fh:
+    urls_header = fh.read()
+
 git_urls = []
 for checkout in checkouts:
     if checkout['payment_status'] == 'paid':
@@ -9,5 +14,12 @@ for checkout in checkouts:
             git_urls.append(checkout['metadata']['github'])
 
 git_urls.reverse()
-for git_url in git_urls:
-    print(git_url)
+with open("project_urls.py", 'w') as fh:
+    fh.write(urls_header)
+
+    for git_url in git_urls:
+        fh.write(f'    "{git_url}",\n')
+
+    fh.write("    ]\n")
+
+print(f"created project_urls.py with {len(git_urls)} projects")
